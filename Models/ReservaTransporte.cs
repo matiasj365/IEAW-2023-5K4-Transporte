2e802d63dbdaa90f36956168d4a7e6783ee6fcf7
Matías Jarab <44157@sistemas.frc.utn.edu.ar>
2023-11-06 19:57:49 -0300

    api/transporte done

--- Models/ReservaTransporte.cs
+++ Models/ReservaTransporte.cs
@@ -6,7 +6,7 @@ public string Descripcion{get;set;}
 public int    Capacidad {get;set;}
 public double    PrecioBase {get;set;}
 
-public Cliente cliente {get;set;}
+public Cliente Cliente {get;set;}
 public DateTime FechaIncio {get;set;}
 public DateTime FechaFin {get;set;}
 public double PrecioTotal {get;set;}
